cb81706b473499a4b8282a5bd4104990c5bee158
willamybraga <willamybraga@yahoo.com>
2022-05-14 08:07:30 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProgramaHexadecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "id=1234#cpf=35312376524#nome=Luis";
+            byte[] bytes = System.Text.Encoding.Default.GetBytes(value);
+            string hexString = BitConverter.ToString(bytes);
+            hexString = hexString.Replace("-", "");
+            Console.WriteLine("Codigo hexadeximal: " + hexString);
+        }
+    }
+}
